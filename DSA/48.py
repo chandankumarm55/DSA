0dfab4933dda78bca4e38760f803b82422ff46bb
Chandan Kumar <149804185+chandankumarm55@users.noreply.github.com>
2024-05-05 20:09:49 +0530

    Create 48.py

---
+++ DSA/48.py
@@ -0,0 +1,25 @@
+'''You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+
+You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.'''
+
+
+def rotate(matrix):
+    n = len(matrix)
+    
+    # Transpose the matrix
+    for i in range(n):
+        for j in range(i, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    
+    # Reverse each row
+    for row in matrix:
+        row.reverse()
+
+# Example usage
+matrix1 = [[1,2,3],[4,5,6],[7,8,9]]
+rotate(matrix1)
+print(matrix1)  # Output: [[7, 4, 1], [8, 5, 2], [9, 6, 3]]
+
+matrix2 = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+rotate(matrix2)
+print(matrix2)  # Output: [[15, 13, 2, 5], [14, 3, 4, 1], [12, 6, 8, 9], [16, 7, 10, 11]]
